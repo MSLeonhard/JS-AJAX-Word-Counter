cbe95e7eefcd4bd2c841451218ad77903708bd44
Michael Leonhard <77648651+MSLeonhard@users.noreply.github.com>
2021-03-03 14:12:24 -0500

    created step by step instructions

---
+++ js/script.js
@@ -0,0 +1,47 @@
+console.log('connected...')
+
+//part 1 steps
+//1. input text file
+//2. iterate through the text file for unique words
+//3. count instances of each unique word
+//4. output words and their amount of times that word appears
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//part 2 steps
+//1. create an object that accepts a text file
+//2. return the word count of the file
+//3. display unique word count
+//4. display most and least used words
+//5. return longest word
+//6. create method that accepts a string, and returns the number of instances of that string in the text file
