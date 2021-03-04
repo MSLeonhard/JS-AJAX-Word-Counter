0fd5bf7974fc80c84d024db0f0b4130059c7067c
Michael Leonhard <77648651+MSLeonhard@users.noreply.github.com>
2021-03-04 14:31:23 -0500

    created words object

--- js/script.js
+++ js/script.js
@@ -8,30 +8,54 @@ console.log('connected...')
 
 let xhr = new XMLHttpRequest();
 
-let parseResponse = function (input) {
-    let noPunctuation = input.replace(/[.,?\/#!$%\^&\*;:{}=\_`~()]/g,"").replace(/-/g, " ").replace(/\n/g, " ");
-    let splitArr = splitString(noPunctuation)
-    let removeEmpty = splitArr.filter(e => e)
-    console.log(removeEmpty)
-}
-
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById('test').innerHTML = this.responseText
-        parseResponse(this.responseText)
-    }
-};
-
 xhr.open('GET', 'https://class-demo-files-and-resources.s3.amazonaws.com/Green-Eggs-And-Ham.txt', true);
 
 xhr.send()
 
-let words = {};
+let parseResponse = function (input) {
+    let noPunctuation = input.replace(/[.,?\/#!$%\^&\*;:{}=\_`~()]/g,"").replace(/-/g, " ").replace(/\n/g, " ");
+    let makeUniform = noPunctuation.toUpperCase()
+    let splitArr = splitString(makeUniform)
+    let removeEmpty = splitArr.filter(e => e) //total word count
+    console.log(removeEmpty)
+    for (i = 0; i<removeEmpty.length; i++) {
+        if (words[removeEmpty[i]]) {
+            words[removeEmpty[i]]++
+        } else {
+            words[removeEmpty[i]] = 1
+        }
+    }
+}
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        parseResponse(this.responseText)
+    }
+};
 
 function splitString (string) {
     let arr = string.split(" ")
     return arr
 }
+
+let words = {
+}
+
+console.log(words)
+
+//iterate through the array with all of our words in it (removeEmpty)
+//create name value pairs for new words with a starting value of 1
+//if a word is already in the object/array, increment its value by 1
+
+//need to return:
+//length of text (remove empty length)
+//unique word count (words object length)
+//most used word (words object pair with highest value)
+//least used word (words object pair with lowest value)
+//longest word (words object key with longest length)
+
+
+
 
 
 //part 2 steps
